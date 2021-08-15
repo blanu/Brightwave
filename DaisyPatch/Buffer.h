b6d7dfdbf23a810b2f8ed2c72581131380442f67
Dr. Brandon Wiley <brandon@operatorfoundation.org>
2021-08-15 14:27:08 -0700

    Added a buffer

---
+++ DaisyPatch/Buffer.h
@@ -0,0 +1,25 @@
+const int bufferSize = 4096;
+
+struct Buffer
+{
+  float buffer[bufferSize];
+  int appendIndex = 0;
+
+  void append(float *values, size_t size)
+  {
+    for(int index = 0; index < size; index++)
+    {
+      if (appendIndex >= bufferSize)
+      {
+        return;
+      }
+      
+      buffer[appendIndex++] = values[index];
+    }
+  }
+
+  bool isFull()
+  {
+    return appendIndex == bufferSize;
+  }
+}
